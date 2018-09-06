f3d23d997d6db4beb99dcb1763f837e32c6d3c1b
Aerron <cs15btech11040@iith.ac.in>
2018-09-06 14:58:47 +0530

    Cp3

--- Cp3/UVa_00927.cpp
+++ Cp3/UVa_00927.cpp
@@ -4,6 +4,41 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	
-	return 0;
+    vector <int> vec;
+    int input, degree, k, d, TC, n, sum, i;
+    long long output;
+    string line;
+    stringstream ss;
+    
+    cin>>TC;
+    while(TC--){
+        vec.clear();
+        
+        // getline(cin, line);
+        // ss.clear();
+        // ss.str(line);
+        // ss>>degree;
+        // while(ss>>input)vec.push_back(input);
+        cin>>degree;
+        degree++;
+        while(degree--){
+            cin>>input;
+            vec.push_back(input);
+        }
+        cin>>d>>k;
+        
+        sum=d;
+        n=1;
+        while(k>sum){
+            n+=1;
+            sum +=n*d;
+        }
+        
+        i = output = 0;
+        for(auto a: vec){
+            output += a*pow(n,i++);
+        }
+        cout<<output<<endl;
+    }
+    return 0;
 }
