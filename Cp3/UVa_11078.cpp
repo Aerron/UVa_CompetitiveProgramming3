8afad4b912190c47a85aab57a7382fc7f617ea87
Aerron <cs15btech11040@iith.ac.in>
2018-09-06 23:35:31 +0530

    Cp3

--- Cp3/UVa_11078.cpp
+++ Cp3/UVa_11078.cpp
@@ -4,6 +4,25 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	
-	return 0;
+    int TC, n, input, df, mx, mn;
+    vector <int> vec;
+    
+    cin>> TC;
+    while(TC--){
+        cin>>n;
+        vec.clear();
+        while(n--){
+            cin>> input;
+            vec.push_back(input);
+        }
+        df = INT_MIN;
+        mx = mn = vec[0];
+        for(auto a= vec.begin()+1; a!=vec.end(); a++){
+            df = max(df, mx-*a );
+            mx= max(mx, *a);
+            mn = min(mn, *a);
+        }
+        cout<< df<< endl;
+    }
+    return 0;
 }
