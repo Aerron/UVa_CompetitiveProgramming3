f34e30ffa7739df5d317f2356bb4c763bb8d031e
Aerron <cs15btech11040@iith.ac.in>
2018-09-06 23:11:57 +0530

    Cp3

--- Cp3/UVa_11001.cpp
+++ Cp3/UVa_11001.cpp
@@ -4,6 +4,25 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	
-	return 0;
+    double v0, v, len, n, len_;
+    bool flag, flag_;
+    while((scanf("%lf %lf", &v, &v0)), (v0>0.0)||(v>0.0) ){
+        len = (0.3 )*pow((v-v0),0.5);
+        flag_ = flag = true;
+        n=1;
+        if(v<v0) flag_ = false;
+        while(flag){
+            n++;
+            len_ =(n)*(0.3)*pow(( (v/n) -v0),0.5);
+            
+            if( (v/n <= v0) ||(len> len_) )flag = false;
+            if( !flag && (fabs(len - len_) <= 1e-12) ) flag_ = false;
+            len =len_;
+            // cout<< len<<endl;
+        }
+        
+        if(flag_) cout<< --n<< endl;
+        else cout<< 0<<endl;
+    }
+    return 0;
 }
