a23f2a9b7dab3592643e5eea459aa8a2dbd3542e
Aerron <cs15btech11040@iith.ac.in>
2018-09-06 14:15:30 +0530

    Cp3

--- Cp3/UVa_00256.cpp
+++ Cp3/UVa_00256.cpp
@@ -3,6 +3,27 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	
-	return 0;
+    vector <vector<int>> vec(4,vector <int>() ) ;
+    
+    int square, input;
+    int lim[4] = {10,100,1000,10000};
+    
+    for(int i=0; i<10000; i++){
+        square = i*i;
+        for(int j=0; j<4; j++){
+            if( (i<lim[j]) && (i == (square/lim[j]+square%lim[j]))){
+                vec[j].push_back(square);
+            }
+        }
+    }
+    
+    while(scanf("%d", &input)!=EOF){
+        for(auto a: vec[input/2-1]){
+        	cout.width(input);
+        	cout.fill( '0' );
+            cout<<a<<right<<endl;
+        }
+    }
+    
+    return 0;
 }
