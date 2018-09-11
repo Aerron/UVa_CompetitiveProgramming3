10d011d54f04ef9b300b6bd8c6d5729375851e8f
Aerron <cs15btech11040@iith.ac.in>
2018-09-12 03:47:39 +0530

    Cp3-2nestedloop

---
+++ Cp3/UVa_10487.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int num, query, input;
+	vector <int> vec;
+	while(scanf("%d",&num), num){
+		while(num--){
+			cin>>input;
+			vec.push_back(input);
+		}
+		sort(vec.begin(), vec.end());
+		cin>>query;
+		while(query--){
+			cin>>input;
+
+				
+		}
+	}
+	return 0;
+}
