1d7595ec6d3dab36e91fa949bc474b8a5647a9e3
Aerron <cs15btech11040@iith.ac.in>
2018-09-12 11:02:16 +0530

    Cp3-2nestedloop

--- Cp3/UVa_10487.cpp
+++ Cp3/UVa_10487.cpp
@@ -3,20 +3,35 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-	int num, query, input;
-	vector <int> vec;
-	while(scanf("%d",&num), num){
-		while(num--){
-			cin>>input;
-			vec.push_back(input);
-		}
-		sort(vec.begin(), vec.end());
-		cin>>query;
-		while(query--){
-			cin>>input;
-
-				
-		}
-	}
-	return 0;
+    int num, query, input, size, min_val, case_val=0;
+    int sum;
+    vector <int> vec;
+    while(scanf("%d",&num), num){
+        case_val++;
+        sum = 0;
+        while(num--){
+            cin>>input;
+            vec.push_back(input);
+        }
+        sort(vec.begin(), vec.end());
+        size = int(vec.size());
+        cin>>query;
+        cout<< "Case "<<case_val<<":"<<endl;
+        while(query--){
+            cin>>input;
+            min_val = INT_MAX;
+            for(int i=0; i<size-1;i++){
+                for (int j=i+1; j<size; j++){
+                    if( abs(input-(vec[i]+vec[j])) < min_val ){
+                        min_val = abs(input-(vec[i]+vec[j]));
+                        sum = vec[i]+vec[j];
+                    }
+                }
+            }
+            cout<< "Closest sum to " <<input<<" is "<<sum<<"."<<endl;
+            
+        }
+        
+    }
+    return 0;
 }
