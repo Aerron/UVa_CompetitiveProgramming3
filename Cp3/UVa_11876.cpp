5371128b07385843bfd35f336735234c54661165
Aerron <cs15btech11040@iith.ac.in>
2018-09-20 10:36:46 +0530

    Divide and Conquer

--- Cp3/UVa_11876.cpp
+++ Cp3/UVa_11876.cpp
@@ -3,15 +3,17 @@ using namespace std;
 
 
 int noffac(int val){
-    int limit, sum = 1;
-
-    for (int i = 2, limit = (int)sqrt(val); i <= limit; i++)
+    int limit, sum = 0;
+    limit = (int)sqrt(val);
+    
+    for (int i = 2; i <= limit; i++)
         if (val%i==0) sum++;
     sum *= 2;
-
+//    cout<<endl;
+//    cout<<limit<<" "<<sqrt(val)<<" "<<val<< " "<<sum<<endl;
     if (limit-sqrt(val)==0) sum--;
-
-    return sum;
+    
+    return sum+2;
 }
 
 
@@ -21,17 +23,25 @@ int main(int argc, char const *argv[])
     int TC, A,B, test_case=0;
     vec.insert(vec.end(),{1, 2, 4, 7, 9, 12, 18});
     int val =18;
-    
+//    for(auto a: vec){
+//        cout<< a<<" ";
+//    }
+//    cout<<endl;
+//    cout<< noffac(100)<<endl;
     while(val<=1000000){
         val = val+ noffac(val);
         vec.push_back(val);
     }
+    sort(vec.begin(), vec.end());
     cin>>TC;
     while(TC--){
         cin>>A>>B;
         auto lower = lower_bound(vec.begin(), vec.end(), A);
         auto upper = upper_bound(vec.begin(), vec.end(), B);
-        cout<< "Case "<< ++test_case<<": " <<upper-lower<<endl;
+        if(upper!=vec.end() && *upper == B)
+            cout<< "Case "<< ++test_case<<": " <<upper-lower+1<<endl;
+        else
+            cout<< "Case "<< ++test_case<<": " <<upper-lower<<endl;
     }
     return 0;
 }
