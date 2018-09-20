8f5062d76de6283600cdaf4819cf3f10c8bea521
Aerron <cs15btech11040@iith.ac.in>
2018-09-20 14:16:09 +0530

    Divide and Conquer

--- Cp3/UVa_10341.cpp
+++ Cp3/UVa_10341.cpp
@@ -18,16 +18,17 @@ int main(int argc, char const *argv[])
     
     while(scanf("%lf %lf %lf %lf %lf %lf", &p, &q, &r, &s, &t, &u)!=EOF){
         lo=0.0;hi=1.0;mid=0.0; ans=0.0;
-        
+		if(p*exp(-1)+q*sin(1)+r*cos(1)+s*tan(1)+t+u>1e-9 || p+r+u<0){
+            printf("No solution\n");
+            continue;
+        }
         while (fabs(hi - lo) > EPS) {
             mid = (lo + hi) / 2.0;
             if (f(lo)*f(mid)<0 ) { ans = mid; hi = mid; }
             else lo = mid;
         }
-        if( fabs(f(ans))<1e-5)
-            printf("%.4lf\n", ans);
-        else
-            cout<< "No solution"<<endl;
+        
+        printf("%.4lf\n", ans);
         
     }
     return 0;
