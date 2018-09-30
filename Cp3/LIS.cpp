f8998c884cc60c637bd308f0ee14cf154bfdb262
Aerron <cs15btech11040@iith.ac.in>
2018-09-30 11:56:00 +0530

    Dynamic Programming

--- Cp3/LIS.cpp
+++ Cp3/LIS.cpp
@@ -15,6 +15,7 @@ int lis(int ind){
     int max_val = 1;
     for (int i = 0; i < ind; ++i) 
     	if(vec[i]<vec[ind]) max_val = max(max_val, 1+lis(i));
+    	else max_val = max(max_val, lis(i));
     return memo[ind]=max_val;
 }
 
