10c14a3fb1f85a5611d937bce55f3d56a31f93c3
Aerron <cs15btech11040@iith.ac.in>
2018-10-02 22:46:25 +0530

    LIS

--- Cp3/LIS.cpp
+++ Cp3/LIS.cpp
@@ -7,17 +7,17 @@ using namespace std;
 vector <int> vec;
 int memo[100];
 
-int lis(int ind){
+// int lis(int ind){
     
-    if(ind<0) return 0;
-    if(memo[ind]!=-1) return memo[ind];
+//     if(ind<0) return 0;
+//     if(memo[ind]!=-1) return memo[ind];
     
-    int max_val = 1;
-    for (int i = 0; i < ind; ++i) 
-    	if(vec[i]<vec[ind]) max_val = max(max_val, 1+lis(i));
-    	else max_val = max(max_val, lis(i));
-    return memo[ind]=max_val;
-}
+//     int max_val = 1;
+//     for (int i = 0; i < ind; ++i) 
+//     	if(vec[i]<vec[ind]) max_val = max(max_val, 1+lis(i));
+//     	else max_val = max(max_val, lis(i));
+//     return memo[ind]=max_val;
+// }
 
 int main(int argc, char const *argv[])
 {
--- Cp3/UVa_00111.cpp
+++ Cp3/UVa_00111.cpp
@@ -1,65 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int rank_map[25], lis[2][25], N, arr[25], max_val;
+long rank_map[25], L[25], N, arr[25], max_val;
 
 
-// int dp(int ind){
-//     if(ind<0) return 0;
-//     if(lis[ind-1]==-1) int prev = dp(ind-1);
-//     if(lis[ind]!=-1) return lis[ind];
-//     max_val = max(max_val, prev);
-//     int ans =1;
-//     for(int i=ind; i>0; i--){
-//         if(arr[i]<arr[ind]) ans = max(ans, 1+dp(i));
-//     }
-//     max_val = max(max_val, ans);
-//     return lis[ind] = ans;
-// }
 
 int main(int argc, char const *argv[])
 {
-    int input, curr, count;
-    scanf("%d", &N);
-    for (int i = 1; i <= N; ++i)
+    memset(L, -1, sizeof L);
+    long input, lis, lis_end;
+    scanf("%ld", &N);
+    for (long i = 1; i <= N; ++i)
     {
         cin>>input;
         rank_map[input] = i;
     }
     
-//    cout<<"test1"<<endl;
-    while(scanf("%d", &input)==1){
-        memset(lis, -1, sizeof lis);
+    while(scanf("%ld", &input)==1){
+        
         arr[1] = rank_map[input];
-        for (int i = 2; i <=N ; ++i)
+        for (long i = 2; i <=N ; ++i)
         {
             cin>>input;
             arr[i]=rank_map[input];
         }
-        max_val = 1;
-        // dp(N);
-        for(int i=1; i<=N; i++){
-            lis[0][i]=1;
+        
+        lis=lis_end=0;
+        for(long i=1; i<=N; i++){
+            long pos = lower_bound(L, L+lis, arr[i])-L;
+            L[pos]=arr[i];
+            if(pos+1>lis){
+                lis=pos+1;
+            }
         }
-        curr=1;
-        count=N;
-//        cout<<"test2"<<endl;
-        for(int j=2; j<=N && count>0; j++){
-            memset(lis[curr], 0, sizeof lis[curr]);
-            count=0;
-
-            for(int i=1; i<=N; i++) if(lis[!curr][i])
-                    for(int k=i+1; k<=N; k++) if(arr[i]<arr[k]){
-                            lis[curr][k]=1;
-                            // max_val=j;
-                            count++;
-                        }
-            
-            curr=!curr;
-            if(count>0) max_val++;
-//            cout<<"test3"<<endl;
-        }
-        cout<<max_val<<endl;
+        cout<<lis<<endl;
+        
+        
         
     }
     
